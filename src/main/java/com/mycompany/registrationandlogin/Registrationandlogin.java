dcaa40e8c5d7d0757b203d243dd4bcff73a35d17
RC_Student_lab <RC_Student_lab@10.0.0.66>
2024-10-01 21:57:28 +0000

    First commit

---
+++ src/main/java/com/mycompany/registrationandlogin/Registrationandlogin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registrationandlogin;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Registrationandlogin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
